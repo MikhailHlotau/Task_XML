00aea774cfb98f037e7abce78e2e2135049c566b
mikle007 <Mikle007@bitbucket.org>
2023-05-25 19:25:04 +0300

    TaskXML

---
+++ src/main/java/com/glotov/xml/parsers/XmlParserFactory.java
@@ -0,0 +1,19 @@
+package com.glotov.xml.parsers;
+
+import com.glotov.xml.parsers.impl.DomXmlParser;
+import com.glotov.xml.parsers.impl.SaxXmlParser;
+import com.glotov.xml.parsers.impl.StaxXmlParser;
+
+public class XmlParserFactory {
+    public static XmlParser createDomXmlParser() {
+        return new DomXmlParser();
+    }
+
+    public static XmlParser createSaxXmlParser() {
+        return new SaxXmlParser();
+    }
+
+    public static XmlParser createStaxXmlParser() {
+        return new StaxXmlParser();
+    }
+}
